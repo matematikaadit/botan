89f51dc9e5baab3f7363ff9215b4f649d9dd9244
Adit Cahya Ramadhan <matematika.adit@gmail.com>
2017-01-31 14:30:38 +0700

    fix database

--- main.js
+++ main.js
@@ -3,10 +3,6 @@ const env = require('./env');
 const request = require('request');
 const fs = require('fs');
 
-const gist = 'https://gist.githubusercontent.com/matematikaadit/19a16a452496334cb823893aaf620d08/raw/3cfc4770c57156219e4ace7e0a5c9df15bcc5966/botan.db';
-request(gist).pipe(fs.createWriteStream('.data/botan.db'));
-
-
 const website = require('./website');
 website.start();
 
